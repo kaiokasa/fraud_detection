0b7596d7555af147c4800c35942b97bacef2b636
Bouchentouf Omar <bouchentoufomar05@gmail.com>
2026-03-15 17:51:35 +0100

    Finished the unbalanced modeling with multiple models, and created file for the next step (balanced modeling)

--- crossval_report.py
+++ crossval_report.py
@@ -1,10 +1,9 @@
-from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_predict
 
 def print_crossval_results(model, X_train, y_train, cv=5):
 
     y_pred_cv = cross_val_predict(model, X_train, y_train, cv=cv)
-    y_proba_cv = cross_val_predict(model, X_train, y_train, cv=cv, method='predict_proba')[:, 1]
 
     report_cv = classification_report(y_train, y_pred_cv, output_dict=True)
 
@@ -13,4 +12,3 @@ def print_crossval_results(model, X_train, y_train, cv=5):
           f"Recall={report_cv['0']['recall']:.3f}, F1={report_cv['0']['f1-score']:.3f}")
     print(f"Class 1 (Fraud)    : Precision={report_cv['1']['precision']:.3f}, "
           f"Recall={report_cv['1']['recall']:.3f}, F1={report_cv['1']['f1-score']:.3f}")
-    print(f"ROC-AUC: {roc_auc_score(y_train, y_proba_cv):.3f}\n")
